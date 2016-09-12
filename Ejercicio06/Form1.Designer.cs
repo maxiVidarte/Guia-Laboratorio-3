3f270d7f799da54647bdc955d9c39270e1979354
maxiVidarte <maxiavidarte@gmail.com>
2016-09-12 19:49:18 -0300

    Ejercicio06 mejorado

--- Ejercicio06/Form1.Designer.cs
+++ Ejercicio06/Form1.Designer.cs
@@ -30,21 +30,21 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
             this.gpb_dinero = new System.Windows.Forms.GroupBox();
-            this.gpb_Billetes = new System.Windows.Forms.GroupBox();
-            this.lbl_dinero = new System.Windows.Forms.Label();
             this.txt_dinero = new System.Windows.Forms.TextBox();
-            this.lbl_billete2 = new System.Windows.Forms.Label();
-            this.lbl_billete5 = new System.Windows.Forms.Label();
-            this.lbl_billete10 = new System.Windows.Forms.Label();
-            this.lbl_billete20 = new System.Windows.Forms.Label();
-            this.lbl_billete50 = new System.Windows.Forms.Label();
-            this.lbl_billete100 = new System.Windows.Forms.Label();
-            this.txt_billete2 = new System.Windows.Forms.TextBox();
-            this.txt_billete5 = new System.Windows.Forms.TextBox();
-            this.txt_billete10 = new System.Windows.Forms.TextBox();
-            this.txt_billete20 = new System.Windows.Forms.TextBox();
-            this.txt_billete50 = new System.Windows.Forms.TextBox();
+            this.lbl_dinero = new System.Windows.Forms.Label();
+            this.gpb_Billetes = new System.Windows.Forms.GroupBox();
             this.txt_billete100 = new System.Windows.Forms.TextBox();
+            this.txt_billete50 = new System.Windows.Forms.TextBox();
+            this.txt_billete20 = new System.Windows.Forms.TextBox();
+            this.txt_billete10 = new System.Windows.Forms.TextBox();
+            this.txt_billete5 = new System.Windows.Forms.TextBox();
+            this.txt_billete2 = new System.Windows.Forms.TextBox();
+            this.lbl_billete100 = new System.Windows.Forms.Label();
+            this.lbl_billete50 = new System.Windows.Forms.Label();
+            this.lbl_billete20 = new System.Windows.Forms.Label();
+            this.lbl_billete10 = new System.Windows.Forms.Label();
+            this.lbl_billete5 = new System.Windows.Forms.Label();
+            this.lbl_billete2 = new System.Windows.Forms.Label();
             this.btn_Aceptar = new System.Windows.Forms.Button();
             this.btn_Limpiar = new System.Windows.Forms.Button();
             this.btn_Salir = new System.Windows.Forms.Button();
@@ -62,6 +62,23 @@
             this.gpb_dinero.TabIndex = 0;
             this.gpb_dinero.TabStop = false;
             this.gpb_dinero.Text = "Cantidad a Retirar";
+            // 
+            // txt_dinero
+            // 
+            this.txt_dinero.Location = new System.Drawing.Point(105, 40);
+            this.txt_dinero.Name = "txt_dinero";
+            this.txt_dinero.Size = new System.Drawing.Size(100, 20);
+            this.txt_dinero.TabIndex = 1;
+            this.txt_dinero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_dinero_KeyPress);
+            // 
+            // lbl_dinero
+            // 
+            this.lbl_dinero.AutoSize = true;
+            this.lbl_dinero.Location = new System.Drawing.Point(6, 43);
+            this.lbl_dinero.Name = "lbl_dinero";
+            this.lbl_dinero.Size = new System.Drawing.Size(93, 13);
+            this.lbl_dinero.TabIndex = 0;
+            this.lbl_dinero.Text = "Cantidad a retirar :";
             // 
             // gpb_Billetes
             // 
@@ -84,112 +101,14 @@
             this.gpb_Billetes.TabStop = false;
             this.gpb_Billetes.Text = "Cantidad De Billetes";
             // 
-            // lbl_dinero
+            // txt_billete100
             // 
-            this.lbl_dinero.AutoSize = true;
-            this.lbl_dinero.Location = new System.Drawing.Point(6, 43);
-            this.lbl_dinero.Name = "lbl_dinero";
-            this.lbl_dinero.Size = new System.Drawing.Size(93, 13);
-            this.lbl_dinero.TabIndex = 0;
-            this.lbl_dinero.Text = "Cantidad a retirar :";
-            // 
-            // txt_dinero
-            // 
-            this.txt_dinero.Location = new System.Drawing.Point(105, 40);
-            this.txt_dinero.Name = "txt_dinero";
-            this.txt_dinero.Size = new System.Drawing.Size(100, 20);
-            this.txt_dinero.TabIndex = 1;
-            this.txt_dinero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_dinero_KeyPress);
-            // 
-            // lbl_billete2
-            // 
-            this.lbl_billete2.AutoSize = true;
-            this.lbl_billete2.Location = new System.Drawing.Point(23, 38);
-            this.lbl_billete2.Name = "lbl_billete2";
-            this.lbl_billete2.Size = new System.Drawing.Size(76, 13);
-            this.lbl_billete2.TabIndex = 0;
-            this.lbl_billete2.Text = "Billetes de $2 :";
-            // 
-            // lbl_billete5
-            // 
-            this.lbl_billete5.AutoSize = true;
-            this.lbl_billete5.Location = new System.Drawing.Point(23, 64);
-            this.lbl_billete5.Name = "lbl_billete5";
-            this.lbl_billete5.Size = new System.Drawing.Size(76, 13);
-            this.lbl_billete5.TabIndex = 1;
-            this.lbl_billete5.Text = "Billetes de $5 :";
-            // 
-            // lbl_billete10
-            // 
-            this.lbl_billete10.AutoSize = true;
-            this.lbl_billete10.Location = new System.Drawing.Point(17, 90);
-            this.lbl_billete10.Name = "lbl_billete10";
-            this.lbl_billete10.Size = new System.Drawing.Size(82, 13);
-            this.lbl_billete10.TabIndex = 2;
-            this.lbl_billete10.Text = "Billetes de $10 :";
-            // 
-            // lbl_billete20
-            // 
-            this.lbl_billete20.AutoSize = true;
-            this.lbl_billete20.Location = new System.Drawing.Point(17, 116);
-            this.lbl_billete20.Name = "lbl_billete20";
-            this.lbl_billete20.Size = new System.Drawing.Size(82, 13);
-            this.lbl_billete20.TabIndex = 3;
-            this.lbl_billete20.Text = "Billetes de $20 :";
-            // 
-            // lbl_billete50
-            // 
-            this.lbl_billete50.AutoSize = true;
-            this.lbl_billete50.Location = new System.Drawing.Point(17, 142);
-            this.lbl_billete50.Name = "lbl_billete50";
-            this.lbl_billete50.Size = new System.Drawing.Size(82, 13);
-            this.lbl_billete50.TabIndex = 4;
-            this.lbl_billete50.Text = "Billetes de $50 :";
-            // 
-            // lbl_billete100
-            // 
-            this.lbl_billete100.AutoSize = true;
-            this.lbl_billete100.Location = new System.Drawing.Point(11, 168);
-            this.lbl_billete100.Name = "lbl_billete100";
-            this.lbl_billete100.Size = new System.Drawing.Size(88, 13);
-            this.lbl_billete100.TabIndex = 5;
-            this.lbl_billete100.Text = "Billetes de $100 :";
-            // 
-            // txt_billete2
-            // 
-            this.txt_billete2.Enabled = false;
-            this.txt_billete2.Location = new System.Drawing.Point(105, 35);
-            this.txt_billete2.Name = "txt_billete2";
-            this.txt_billete2.Size = new System.Drawing.Size(100, 20);
-            this.txt_billete2.TabIndex = 6;
-            this.txt_billete2.Text = "0";
-            // 
-            // txt_billete5
-            // 
-            this.txt_billete5.Enabled = false;
-            this.txt_billete5.Location = new System.Drawing.Point(105, 61);
-            this.txt_billete5.Name = "txt_billete5";
-            this.txt_billete5.Size = new System.Drawing.Size(100, 20);
-            this.txt_billete5.TabIndex = 7;
-            this.txt_billete5.Text = "0";
-            // 
-            // txt_billete10
-            // 
-            this.txt_billete10.Enabled = false;
-            this.txt_billete10.Location = new System.Drawing.Point(105, 87);
-            this.txt_billete10.Name = "txt_billete10";
-            this.txt_billete10.Size = new System.Drawing.Size(100, 20);
-            this.txt_billete10.TabIndex = 8;
-            this.txt_billete10.Text = "0";
-            // 
-            // txt_billete20
-            // 
-            this.txt_billete20.Enabled = false;
-            this.txt_billete20.Location = new System.Drawing.Point(105, 113);
-            this.txt_billete20.Name = "txt_billete20";
-            this.txt_billete20.Size = new System.Drawing.Size(100, 20);
-            this.txt_billete20.TabIndex = 9;
-            this.txt_billete20.Text = "0";
+            this.txt_billete100.Enabled = false;
+            this.txt_billete100.Location = new System.Drawing.Point(105, 165);
+            this.txt_billete100.Name = "txt_billete100";
+            this.txt_billete100.Size = new System.Drawing.Size(100, 20);
+            this.txt_billete100.TabIndex = 11;
+            this.txt_billete100.Text = "0";
             // 
             // txt_billete50
             // 
@@ -200,14 +119,95 @@
             this.txt_billete50.TabIndex = 10;
             this.txt_billete50.Text = "0";
             // 
-            // txt_billete100
+            // txt_billete20
             // 
-            this.txt_billete100.Enabled = false;
-            this.txt_billete100.Location = new System.Drawing.Point(105, 165);
-            this.txt_billete100.Name = "txt_billete100";
-            this.txt_billete100.Size = new System.Drawing.Size(100, 20);
-            this.txt_billete100.TabIndex = 11;
-            this.txt_billete100.Text = "0";
+            this.txt_billete20.Enabled = false;
+            this.txt_billete20.Location = new System.Drawing.Point(105, 113);
+            this.txt_billete20.Name = "txt_billete20";
+            this.txt_billete20.Size = new System.Drawing.Size(100, 20);
+            this.txt_billete20.TabIndex = 9;
+            this.txt_billete20.Text = "0";
+            // 
+            // txt_billete10
+            // 
+            this.txt_billete10.Enabled = false;
+            this.txt_billete10.Location = new System.Drawing.Point(105, 87);
+            this.txt_billete10.Name = "txt_billete10";
+            this.txt_billete10.Size = new System.Drawing.Size(100, 20);
+            this.txt_billete10.TabIndex = 8;
+            this.txt_billete10.Text = "0";
+            // 
+            // txt_billete5
+            // 
+            this.txt_billete5.Enabled = false;
+            this.txt_billete5.Location = new System.Drawing.Point(105, 61);
+            this.txt_billete5.Name = "txt_billete5";
+            this.txt_billete5.Size = new System.Drawing.Size(100, 20);
+            this.txt_billete5.TabIndex = 7;
+            this.txt_billete5.Text = "0";
+            // 
+            // txt_billete2
+            // 
+            this.txt_billete2.Enabled = false;
+            this.txt_billete2.Location = new System.Drawing.Point(105, 35);
+            this.txt_billete2.Name = "txt_billete2";
+            this.txt_billete2.Size = new System.Drawing.Size(100, 20);
+            this.txt_billete2.TabIndex = 6;
+            this.txt_billete2.Text = "0";
+            // 
+            // lbl_billete100
+            // 
+            this.lbl_billete100.AutoSize = true;
+            this.lbl_billete100.Location = new System.Drawing.Point(11, 168);
+            this.lbl_billete100.Name = "lbl_billete100";
+            this.lbl_billete100.Size = new System.Drawing.Size(88, 13);
+            this.lbl_billete100.TabIndex = 5;
+            this.lbl_billete100.Text = "Billetes de $100 :";
+            // 
+            // lbl_billete50
+            // 
+            this.lbl_billete50.AutoSize = true;
+            this.lbl_billete50.Location = new System.Drawing.Point(17, 142);
+            this.lbl_billete50.Name = "lbl_billete50";
+            this.lbl_billete50.Size = new System.Drawing.Size(82, 13);
+            this.lbl_billete50.TabIndex = 4;
+            this.lbl_billete50.Text = "Billetes de $50 :";
+            // 
+            // lbl_billete20
+            // 
+            this.lbl_billete20.AutoSize = true;
+            this.lbl_billete20.Location = new System.Drawing.Point(17, 116);
+            this.lbl_billete20.Name = "lbl_billete20";
+            this.lbl_billete20.Size = new System.Drawing.Size(82, 13);
+            this.lbl_billete20.TabIndex = 3;
+            this.lbl_billete20.Text = "Billetes de $20 :";
+            // 
+            // lbl_billete10
+            // 
+            this.lbl_billete10.AutoSize = true;
+            this.lbl_billete10.Location = new System.Drawing.Point(17, 90);
+            this.lbl_billete10.Name = "lbl_billete10";
+            this.lbl_billete10.Size = new System.Drawing.Size(82, 13);
+            this.lbl_billete10.TabIndex = 2;
+            this.lbl_billete10.Text = "Billetes de $10 :";
+            // 
+            // lbl_billete5
+            // 
+            this.lbl_billete5.AutoSize = true;
+            this.lbl_billete5.Location = new System.Drawing.Point(23, 64);
+            this.lbl_billete5.Name = "lbl_billete5";
+            this.lbl_billete5.Size = new System.Drawing.Size(76, 13);
+            this.lbl_billete5.TabIndex = 1;
+            this.lbl_billete5.Text = "Billetes de $5 :";
+            // 
+            // lbl_billete2
+            // 
+            this.lbl_billete2.AutoSize = true;
+            this.lbl_billete2.Location = new System.Drawing.Point(23, 38);
+            this.lbl_billete2.Name = "lbl_billete2";
+            this.lbl_billete2.Size = new System.Drawing.Size(76, 13);
+            this.lbl_billete2.TabIndex = 0;
+            this.lbl_billete2.Text = "Billetes de $2 :";
             // 
             // btn_Aceptar
             // 
@@ -217,7 +217,6 @@
             this.btn_Aceptar.TabIndex = 1;
             this.btn_Aceptar.Text = "&Aceptar";
             this.btn_Aceptar.UseVisualStyleBackColor = true;
-            this.btn_Aceptar.Click += new System.EventHandler(this.btn_Aceptar_Click);
             // 
             // btn_Limpiar
             // 
@@ -227,7 +226,6 @@
             this.btn_Limpiar.TabIndex = 2;
             this.btn_Limpiar.Text = "&Limpiar";
             this.btn_Limpiar.UseVisualStyleBackColor = true;
-            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
             // 
             // btn_Salir
             // 
@@ -237,7 +235,6 @@
             this.btn_Salir.TabIndex = 3;
             this.btn_Salir.Text = "&Salir";
             this.btn_Salir.UseVisualStyleBackColor = true;
-            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
             // 
             // frmPrincipal
             // 
@@ -253,6 +250,7 @@
             this.Name = "frmPrincipal";
             this.Text = "Cajero Automatico";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmPrincipal_FormClosing_1);
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
             this.gpb_dinero.ResumeLayout(false);
             this.gpb_dinero.PerformLayout();
             this.gpb_Billetes.ResumeLayout(false);
