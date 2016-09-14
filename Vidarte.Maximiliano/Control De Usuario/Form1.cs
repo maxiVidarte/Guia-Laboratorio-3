324040b6bc5fe1f687fcef0e25eb0085b7193432
maxiVidarte <maxiavidarte@gmail.com>
2016-09-13 22:08:31 -0300

    se agregaron de la clase 5

---
+++ Vidarte.Maximiliano/Control De Usuario/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+namespace Control_De_Usuario
+{
+    public partial class Form1 : Form
+    {
+
+        MiEntidad[] miArray = new MiEntidad[10];
+        MiEntidad unE = new MiEntidad(2, "pepito", 15);
+        MiEntidad dosE = new MiEntidad(3, "petraccio", 20);
+        MiEntidad tresE = new MiEntidad(2, "pedro", 20);
+        MiEntidad cuatroE = new MiEntidad(1, "beto", 10);
+        MiEntidad cincoE = new MiEntidad(5, "hola", 40);
+        MiEntidad seisE = new MiEntidad(6, "mundo", 50);
+        MiEntidad sieteE = new MiEntidad(7, "carlo", 2);
+        MiEntidad ochoE = new MiEntidad(0, "n n ", 20);
+        MiEntidad nueveE = new MiEntidad(9, "yui", 90);
+        MiEntidad diezE = new MiEntidad(70, "erdfs", 40);
+        
+        public void Ordenamiento()
+        {
+            Array.Sort(miArray, MiEntidad.ComparaAtributo);
+             
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        
+            this.miArray.SetValue(unE, 0);
+            this.miArray.SetValue(dosE, 1);
+            this.miArray.SetValue(tresE, 2);
+            this.miArray.SetValue(cuatroE,3);
+            this.miArray.SetValue(cincoE,4);
+            this.miArray.SetValue(seisE,5);
+            this.miArray.SetValue(sieteE,6);
+            this.miArray.SetValue(ochoE, 7);
+            this.miArray.SetValue(nueveE, 8);
+            this.miArray.SetValue(diezE, 9);
+            this.Ordenamiento();
+            this.lst_lista.Items.AddRange(miArray);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(miArray.GetValue(0).ToString(), "Informacion");
+        }
+    }
+}
---
+++ Vidarte.Maximiliano/Entidades/MiEntidad.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class MiEntidad
+    {
+        private int _numero;
+        private string _nombre;
+        private float _valor;
+
+        public MiEntidad(int num, string nom, float val)
+        {
+            this._nombre = nom;
+            this._numero = num;
+            this._valor = val;
+        }
+            public int Numero 
+        { 
+            get{return this._numero;}
+            set { this._numero = value; }
+        }
+        public string Nombre 
+        { 
+            get { return this._nombre; }
+            set { this._nombre = value; }
+        }
+        public float Valor
+        { 
+            get { return this._valor; } 
+            set { this._valor = value; } 
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("numero: ");
+            sb.AppendLine(this._numero.ToString());
+            sb.Append("Nombre: ");
+            sb.AppendLine(this._nombre);
+            sb.Append("Valor: ");
+            sb.AppendLine(this._valor.ToString());
+            return sb.ToString();
+        }
+
+        public delegate int Comparison<in MiEntidad>(MiEntidad uno, MiEntidad dos);
+
+        public static int ComparaAtributo(MiEntidad a, MiEntidad b)
+        {
+            if (a._numero > b._numero)
+                return 1;
+            else if (a._numero == b._numero)
+                return 0;
+            else
+                return -1;
+
+        }
+
+    }
+}
