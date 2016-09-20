dced67e73c9485bc73f46f198ae6a9282e7123b7
maxiVidarte <maxiavidarte@gmail.com>
2016-09-20 19:48:57 -0300

    primera parte del parcial terminado

--- EjercicioParcial/PrimerParcial2016/Entidades/Medico.cs
+++ EjercicioParcial/PrimerParcial2016/Entidades/Medico.cs
@@ -8,6 +8,29 @@ namespace Entidades
 {
     public class Medico
     {
-       
+        private eEspecialidades _especialidad;
+        private DateTime _horarioEntrada;
+        private string _legajo;
+        private string _nombre;
+
+        public eEspecialidades Especialidad { get { return this._especialidad; } set { this._especialidad = value; } }
+        public DateTime Ingreso { get { return this._horarioEntrada; } set { this._horarioEntrada = value; } }
+        public string Legajo { get { return this._legajo; } set { this._legajo= value; }}
+        public string Nombre { get { return this._nombre; } set { this._nombre = value; } }
+
+        public Medico(string nombre, string legajo, eEspecialidades especialidad)
+        {
+            this._nombre = nombre;
+            this._legajo = legajo;
+            this._especialidad = especialidad;
+        }
+        public int OrdenarPorHorarioEntrada(Medico m1, Medico m2)
+        {
+           return DateTime.Compare(m1._horarioEntrada, m2._horarioEntrada);
+        }
+        public int OrdenarPorLegajo(Medico m1, Medico m2)
+        {
+           return string.Compare(m1._legajo, m2._legajo);
+        }
     }
 }
--- EjercicioParcial/PrimerParcial2016/Entidades/MedicoSalida.cs
+++ EjercicioParcial/PrimerParcial2016/Entidades/MedicoSalida.cs
@@ -8,5 +8,30 @@ namespace Entidades
 {
     class MedicoSalida:Medico
     {
+        private double _salario;
+        private DateTime Egreso;
+
+        public double Salario { get { return this.CalcularSalario(); } }
+        public DateTime _horarioSalida { get { return this.Egreso; } set { this.Egreso = value; } }
+
+        public MedicoSalida(Medico unMedico):base(unMedico.Nombre,unMedico.Legajo,unMedico.Especialidad)
+        {
+            this.Egreso = DateTime.Now;
+        }
+
+        private double CalcularSalario()
+        {
+            TimeSpan intervalo1 = this.Egreso.Subtract(base.Ingreso);
+            if (base.Especialidad == eEspecialidades.Cardiologo)
+            {
+                return intervalo1.Minutes * 55;
+            }
+            else if (base.Especialidad == eEspecialidades.Clinico)
+            {
+                return intervalo1.Minutes * 50;
+            }
+            else
+                return intervalo1.Minutes * 45;
+        }
     }
 }
