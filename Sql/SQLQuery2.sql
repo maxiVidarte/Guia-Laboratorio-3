9c76a2ef934038d00285ce6ec0140b5ff265dc1a
maxiVidarte <maxiavidarte@gmail.com>
2016-10-17 23:06:54 -0300

    se archivaron las consultas

---
+++ Sql/SQLQuery2.sql
@@ -0,0 +1,11 @@
+/*1)*/	
+select * from dbo.Productos ordenby Order by pNombre 
+
+/*2)*/
+select * from dbo.Proveedores where Localidad = 'Capital'
+
+/*3)*/
+select * from dbo.Envios where Cantidad >= 200 and Cantidad <= 300
+
+/*4)*/
+
