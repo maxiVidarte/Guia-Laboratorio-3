11a4b98c0c99af96902593773a16d0821140fe90
maxiVidarte <maxiavidarte@gmail.com>
2016-11-13 14:15:56 -0300

    ejercicio a medio terminar

--- EjerciciosConSql/Vidarte.Maximiliano.Sql(2)/Vidarte.Maximiliano/Form1.Designer.cs
+++ EjerciciosConSql/Vidarte.Maximiliano.Sql(2)/Vidarte.Maximiliano/Form1.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.Gb_ingresodatos = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.rdb_masc = new System.Windows.Forms.RadioButton();
             this.rdb_Fem = new System.Windows.Forms.RadioButton();
             this.txt_localidad = new System.Windows.Forms.TextBox();
             this.txt_direccion = new System.Windows.Forms.TextBox();
             this.txt_Alumno = new System.Windows.Forms.TextBox();
-            this.txt_Fecha = new System.Windows.Forms.TextBox();
             this.txt_curso = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -43,13 +43,19 @@
             this.label2 = new System.Windows.Forms.Label();
             this.lbl_curso = new System.Windows.Forms.Label();
             this.Gb__opciones = new System.Windows.Forms.GroupBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_guardar = new System.Windows.Forms.Button();
             this.Gb_cursos = new System.Windows.Forms.GroupBox();
             this.Lst_Cursos = new System.Windows.Forms.ListBox();
             this.Gb_Localidad = new System.Windows.Forms.GroupBox();
             this.lst_Localidad = new System.Windows.Forms.ListBox();
+            this.btn_mostrar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_ = new System.Windows.Forms.Button();
+            this.txt_IngresoLocalidad = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.Gb_ingresodatos.SuspendLayout();
             this.Gb__opciones.SuspendLayout();
             this.Gb_cursos.SuspendLayout();
@@ -58,12 +64,12 @@
             // 
             // Gb_ingresodatos
             // 
+            this.Gb_ingresodatos.Controls.Add(this.dateTimePicker1);
             this.Gb_ingresodatos.Controls.Add(this.rdb_masc);
             this.Gb_ingresodatos.Controls.Add(this.rdb_Fem);
             this.Gb_ingresodatos.Controls.Add(this.txt_localidad);
             this.Gb_ingresodatos.Controls.Add(this.txt_direccion);
             this.Gb_ingresodatos.Controls.Add(this.txt_Alumno);
-            this.Gb_ingresodatos.Controls.Add(this.txt_Fecha);
             this.Gb_ingresodatos.Controls.Add(this.txt_curso);
             this.Gb_ingresodatos.Controls.Add(this.label1);
             this.Gb_ingresodatos.Controls.Add(this.label5);
@@ -74,10 +80,18 @@
             this.Gb_ingresodatos.ForeColor = System.Drawing.Color.Blue;
             this.Gb_ingresodatos.Location = new System.Drawing.Point(12, 12);
             this.Gb_ingresodatos.Name = "Gb_ingresodatos";
-            this.Gb_ingresodatos.Size = new System.Drawing.Size(317, 204);
+            this.Gb_ingresodatos.Size = new System.Drawing.Size(332, 204);
             this.Gb_ingresodatos.TabIndex = 0;
             this.Gb_ingresodatos.TabStop = false;
             this.Gb_ingresodatos.Text = "Ingreso de datos";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(127, 56);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(196, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // rdb_masc
             // 
@@ -106,36 +120,29 @@
             this.txt_localidad.Enabled = false;
             this.txt_localidad.Location = new System.Drawing.Point(127, 173);
             this.txt_localidad.Name = "txt_localidad";
-            this.txt_localidad.Size = new System.Drawing.Size(172, 20);
+            this.txt_localidad.Size = new System.Drawing.Size(196, 20);
             this.txt_localidad.TabIndex = 10;
             // 
             // txt_direccion
             // 
             this.txt_direccion.Location = new System.Drawing.Point(127, 143);
             this.txt_direccion.Name = "txt_direccion";
-            this.txt_direccion.Size = new System.Drawing.Size(172, 20);
+            this.txt_direccion.Size = new System.Drawing.Size(196, 20);
             this.txt_direccion.TabIndex = 9;
             // 
             // txt_Alumno
             // 
             this.txt_Alumno.Location = new System.Drawing.Point(127, 84);
             this.txt_Alumno.Name = "txt_Alumno";
-            this.txt_Alumno.Size = new System.Drawing.Size(172, 20);
+            this.txt_Alumno.Size = new System.Drawing.Size(196, 20);
             this.txt_Alumno.TabIndex = 8;
-            // 
-            // txt_Fecha
-            // 
-            this.txt_Fecha.Location = new System.Drawing.Point(127, 53);
-            this.txt_Fecha.Name = "txt_Fecha";
-            this.txt_Fecha.Size = new System.Drawing.Size(172, 20);
-            this.txt_Fecha.TabIndex = 7;
             // 
             // txt_curso
             // 
             this.txt_curso.Enabled = false;
             this.txt_curso.Location = new System.Drawing.Point(127, 25);
             this.txt_curso.Name = "txt_curso";
-            this.txt_curso.Size = new System.Drawing.Size(172, 20);
+            this.txt_curso.Size = new System.Drawing.Size(196, 20);
             this.txt_curso.TabIndex = 6;
             // 
             // label1
@@ -200,47 +207,49 @@
             // 
             // Gb__opciones
             // 
-            this.Gb__opciones.Controls.Add(this.button3);
-            this.Gb__opciones.Controls.Add(this.button2);
-            this.Gb__opciones.Controls.Add(this.button1);
+            this.Gb__opciones.Controls.Add(this.btn_salir);
+            this.Gb__opciones.Controls.Add(this.btn_limpiar);
+            this.Gb__opciones.Controls.Add(this.btn_guardar);
             this.Gb__opciones.ForeColor = System.Drawing.Color.Blue;
-            this.Gb__opciones.Location = new System.Drawing.Point(335, 12);
+            this.Gb__opciones.Location = new System.Drawing.Point(350, 12);
             this.Gb__opciones.Name = "Gb__opciones";
             this.Gb__opciones.Size = new System.Drawing.Size(139, 204);
             this.Gb__opciones.TabIndex = 0;
             this.Gb__opciones.TabStop = false;
             this.Gb__opciones.Text = "Opciones";
             // 
-            // button3
+            // btn_salir
             // 
-            this.button3.ForeColor = System.Drawing.Color.Red;
-            this.button3.Location = new System.Drawing.Point(6, 132);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(127, 41);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Salir";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.btn_salir.ForeColor = System.Drawing.Color.Red;
+            this.btn_salir.Location = new System.Drawing.Point(6, 132);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(127, 41);
+            this.btn_salir.TabIndex = 2;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button2
+            // btn_limpiar
             // 
-            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.button2.Location = new System.Drawing.Point(6, 73);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(127, 41);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Limpiar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btn_limpiar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_limpiar.Location = new System.Drawing.Point(6, 73);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(127, 41);
+            this.btn_limpiar.TabIndex = 1;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
             // 
-            // button1
+            // btn_guardar
             // 
-            this.button1.ForeColor = System.Drawing.Color.Gray;
-            this.button1.Location = new System.Drawing.Point(6, 19);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 41);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Guardar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_guardar.ForeColor = System.Drawing.Color.Gray;
+            this.btn_guardar.Location = new System.Drawing.Point(6, 19);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(127, 41);
+            this.btn_guardar.TabIndex = 0;
+            this.btn_guardar.Text = "Guardar";
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            this.btn_guardar.Click += new System.EventHandler(this.button1_Click);
             // 
             // Gb_cursos
             // 
@@ -282,11 +291,69 @@
             this.lst_Localidad.TabIndex = 0;
             this.lst_Localidad.SelectedIndexChanged += new System.EventHandler(this.lst_Localidad_SelectedIndexChanged);
             // 
+            // btn_mostrar
+            // 
+            this.btn_mostrar.Location = new System.Drawing.Point(495, 16);
+            this.btn_mostrar.Name = "btn_mostrar";
+            this.btn_mostrar.Size = new System.Drawing.Size(178, 41);
+            this.btn_mostrar.TabIndex = 1;
+            this.btn_mostrar.Text = "todos los alumnos";
+            this.btn_mostrar.UseVisualStyleBackColor = true;
+            this.btn_mostrar.Click += new System.EventHandler(this.btn_mostrar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(495, 201);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(850, 134);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(679, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "todos los alumnos con antiguedad superior a dos años";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btn_
+            // 
+            this.btn_.Location = new System.Drawing.Point(499, 71);
+            this.btn_.Name = "btn_";
+            this.btn_.Size = new System.Drawing.Size(174, 41);
+            this.btn_.TabIndex = 4;
+            this.btn_.Text = "alumnos por localidad ingresada mediante un inputbox";
+            this.btn_.UseVisualStyleBackColor = true;
+            // 
+            // txt_IngresoLocalidad
+            // 
+            this.txt_IngresoLocalidad.Location = new System.Drawing.Point(679, 82);
+            this.txt_IngresoLocalidad.Name = "txt_IngresoLocalidad";
+            this.txt_IngresoLocalidad.Size = new System.Drawing.Size(185, 20);
+            this.txt_IngresoLocalidad.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(499, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 46);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(691, 407);
+            this.ClientSize = new System.Drawing.Size(1371, 407);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txt_IngresoLocalidad);
+            this.Controls.Add(this.btn_);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_mostrar);
             this.Controls.Add(this.Gb__opciones);
             this.Controls.Add(this.Gb_cursos);
             this.Controls.Add(this.Gb_Localidad);
@@ -299,6 +366,7 @@
             this.Gb_cursos.ResumeLayout(false);
             this.Gb_Localidad.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -321,11 +389,17 @@
         private System.Windows.Forms.TextBox txt_localidad;
         private System.Windows.Forms.TextBox txt_direccion;
         private System.Windows.Forms.TextBox txt_Alumno;
-        private System.Windows.Forms.TextBox txt_Fecha;
         private System.Windows.Forms.TextBox txt_curso;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btn_mostrar;
+        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_;
+        private System.Windows.Forms.TextBox txt_IngresoLocalidad;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- EjerciciosConSql/Vidarte.Maximiliano.Sql(2)/Vidarte.Maximiliano/Form1.cs
+++ EjerciciosConSql/Vidarte.Maximiliano.Sql(2)/Vidarte.Maximiliano/Form1.cs
@@ -21,7 +21,9 @@ namespace Vidarte.Maximiliano
         {
             InitializeComponent();
             CargarListboxs();
-
+            CargarDtYDs();
+            this.dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            this.dateTimePicker1.CustomFormat = " dd/mm/yyyy";
         }
 
         private void Lst_Cursos_SelectedIndexChanged(object sender, EventArgs e)
@@ -44,6 +46,7 @@ namespace Vidarte.Maximiliano
             Lst_Cursos.Items.Add("Borland C++");
             Lst_Cursos.Items.Add("Microsoft C#(Sharp) Nivel I");
             Lst_Cursos.Items.Add("Microsoft C#(Sharp) Nivel II");
+            Lst_Cursos.Items.Add("Microsoft Visual Basic .Net Nivel I");
             Lst_Cursos.Items.Add("Microsoft SqlServer 2008 Nivel II");
             Lst_Cursos.Items.Add("Java");
             lst_Localidad.Items.Add("Avellaneda");
@@ -55,14 +58,111 @@ namespace Vidarte.Maximiliano
         }
         public void CargarDtYDs()
         {
+            //creo las columnas de la tabla cursos
             this.dtCurso.Columns.Add("codCurso", typeof(Int32));
             this.dtCurso.Columns.Add("Descripcion", typeof(string));
             this.dtCurso.PrimaryKey = new DataColumn[] { this.dtCurso.Columns["codCurso"] };
-
+            //creo las columnas de la tabla localidades
             this.dtLocalidad.Columns.Add("codLocalidad", typeof(Int32));
             this.dtLocalidad.Columns.Add("Descripcion", typeof(String));
             this.dtLocalidad.PrimaryKey = new DataColumn[] { this.dtLocalidad.Columns["codLocalidad"] };
+            //creo las columnas de la tabla matriculas
+            this.dtMatricula.Columns.Add("codCurso", typeof(Int32));
+            this.dtMatricula.Columns.Add("Fecha", typeof(String));
+            this.dtMatricula.Columns.Add("Alumno", typeof(String));
+            this.dtMatricula.Columns.Add("Sexo", typeof(String));
+            this.dtMatricula.Columns.Add("Direccion", typeof(String));
+            this.dtMatricula.Columns.Add("codLocalidad", typeof(Int32));
+            //agrego las tablas creadas al dataset
+            this.dsInscripcion.Tables.Add(dtCurso);
+            this.dsInscripcion.Tables.Add(dtLocalidad);
+            this.dsInscripcion.Tables.Add(dtMatricula);
+            //creo las relaciones entre las tablas para luego agregarlas al dataset
+            DataRelation mirelacion1 = new DataRelation("Fk_Matricula_Curso", dsInscripcion.Tables["Cursos"].Columns["codCurso"], dsInscripcion.Tables["Matriculas"].Columns["codCurso"]);
+            DataRelation mirelacion2 = new DataRelation("Fk_Matricula_Localidad", dsInscripcion.Tables["Localidades"].Columns["codLocalidad"], dsInscripcion.Tables["Matriculas"].Columns["codLocalidad"]);
+            //Agrego las relaciones
+            this.dsInscripcion.Relations.Add(mirelacion1);
+            this.dsInscripcion.Relations.Add(mirelacion2);
 
+            //creo los registros de Localidad
+            DataRow fila1 = this.dtLocalidad.NewRow(); fila1[0] = 0; fila1[1] = "Avellaneda";
+            DataRow fila2 = this.dtLocalidad.NewRow(); fila2[0] = 1; fila2[1] = "Capital Federal";
+            DataRow fila3 = this.dtLocalidad.NewRow(); fila3[0] = 2; fila3[1] = "Quilmes";
+            DataRow fila4 = this.dtLocalidad.NewRow(); fila4[0] = 3; fila4[1] = "Berazategui";
+            DataRow fila5 = this.dtLocalidad.NewRow(); fila5[0] = 4; fila5[1] = "Lanus";
+            DataRow fila6 = this.dtLocalidad.NewRow(); fila6[0] = 5; fila6[1] = "Banfield";
+            //creo los registros de curso
+            DataRow f1 = this.dtCurso.NewRow(); f1[0] = 0; f1[1] = "Borland C";
+            DataRow f2 = this.dtCurso.NewRow(); f2[0] = 1; f2[1] = "Borland C++";
+            DataRow f3 = this.dtCurso.NewRow(); f3[0] = 2; f3[1] = "Microsoft C# (Sharp) Nivel I";
+            DataRow f4 = this.dtCurso.NewRow(); f4[0] = 3; f4[1] = "Microsoft C# (Sharp) Nivel II";
+            DataRow f5 = this.dtCurso.NewRow(); f5[0] = 4; f5[1] = "Microsoft Visual Basic .Net Nivel I";
+            DataRow f6 = this.dtCurso.NewRow(); f6[0] = 5; f6[1] = "Microsoft SqlServer 2008 Nivel II";
+            DataRow f7 = this.dtCurso.NewRow(); f7[0] = 6; f7[1] = "Java";
+            //Agrego los registros de curso y localidad
+            this.dtCurso.Rows.Add(f1);
+            this.dtCurso.Rows.Add(f2);
+            this.dtCurso.Rows.Add(f3);
+            this.dtCurso.Rows.Add(f4);
+            this.dtCurso.Rows.Add(f5);
+            this.dtCurso.Rows.Add(f6);
+            this.dtCurso.Rows.Add(f7);
+            this.dtLocalidad.Rows.Add(fila1);
+            this.dtLocalidad.Rows.Add(fila2);
+            this.dtLocalidad.Rows.Add(fila3);
+            this.dtLocalidad.Rows.Add(fila4);
+            this.dtLocalidad.Rows.Add(fila5);
+            this.dtLocalidad.Rows.Add(fila6);
         }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            string hola = dateTimePicker1.Value.ToString("dd/mm/yyyy");
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataRow fila = this.dtMatricula.NewRow();
+            fila[0] = this.Lst_Cursos.SelectedIndex;
+            fila[1] = dateTimePicker1.Value.ToString("dd/mm/yyyy");
+            fila[2] = this.txt_Alumno.Text;
+            if (rdb_Fem.Checked == true)
+                fila[3] = "Femenino";
+            else
+                fila[3] = "Masculino";
+            fila[4] = this.txt_direccion.Text;
+            fila[5] = this.lst_Localidad.SelectedIndex;
+            this.dtMatricula.Rows.Add(fila);
+            
+        }
+
+        private void btn_mostrar_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+            foreach (DataRow item in dsInscripcion.Tables["Matriculas"].Rows)
+            {
+                this.listBox1.Items.Add(item["codCurso"].ToString() + " - " + item["Fecha"].ToString() + " - " + item["Alumno"].ToString() + " - " + item["Sexo"].ToString() + " - " + item["Direccion"].ToString() + " - " + item["codLocalidad"].ToString());
+            }
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+           
+            foreach (Control item in this.Gb_ingresodatos.Controls)
+            {
+                if (item is TextBox)
+                {
+                    item.Text = "";
+                }
+                if (item is RadioButton)
+                {
+                    RadioButton item1 = (RadioButton)item;
+                    item1.Checked = false;
+                }
+            }
+        }
+
     }
 }
